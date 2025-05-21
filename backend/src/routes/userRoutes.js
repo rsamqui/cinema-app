395f897b5887eec188e764cac4fa9a34be876c51
Rodolfo Samqui <70598277+rsamqui@users.noreply.github.com>
2025-05-20 18:55:15 -0600

    updateUser permissions changed

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,8 +6,8 @@ const { authenticate, authorize } = require('../middleware/auth');
 router.post('/login', userController.loginUser);
 router.post('/register', userController.createUser);
 router.get('/users/:id', authenticate, authorize(['admin']), userController.getUsers);
-router.get('/users/details/:id', authenticate, authorize(['admin']), userController.getUserById);
-router.put('/users/:id', authenticate, authorize(['admin']), userController.updateUser);
+router.get('/users/details/:id', authenticate, authorize(['admin','client']), userController.getUserById);
+router.put('/users/edit/:id', authenticate, authorize(['admin','client']), userController.updateUser);
 router.delete('/users/:id', authenticate, authorize(['admin']), userController.deleteUser);
 
 module.exports = router;
