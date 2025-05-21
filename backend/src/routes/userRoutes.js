80ff56b90342995cc0dddd99cf38c38c500f94a4
Rodolfo Samqui <70598277+rsamqui@users.noreply.github.com>
2025-05-20 19:06:55 -0600

    User route upfate

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,7 +5,7 @@ const { authenticate, authorize } = require('../middleware/auth');
 
 router.post('/login', userController.loginUser);
 router.post('/register', userController.createUser);
-router.get('/users/:id', authenticate, authorize(['admin']), userController.getUsers);
+router.get('/users/', authenticate, authorize(['admin']), userController.getUsers);
 router.get('/users/details/:id', authenticate, authorize(['admin','client']), userController.getUserById);
 router.put('/users/edit/:id', authenticate, authorize(['admin','client']), userController.updateUser);
 router.delete('/users/:id', authenticate, authorize(['admin']), userController.deleteUser);
